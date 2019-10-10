011a2a6c3029e32f9dacb57591ff33abad2a9bca
sunj95 <sunj95@naver.com>
2019-10-10 19:44:35 +0900

    Add CLCD Comments

--- LAB_ATmega128/USART_LCD/USART_LCD/main.c
+++ LAB_ATmega128/USART_LCD/USART_LCD/main.c
@@ -13,15 +13,15 @@
 #include <util/delay.h>
 #include <string.h>
 
-void USART_Init(unsigned int ubrr){
-	UBRR0H = (unsigned char)(ubrr >> 8);
-	UBRR0L = (unsigned char)ubrr;
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-	UCSR0C = (3 << UCSZ0);
+void USART_Init(unsigned int ubrr){ // Set USART0 Registers
+	UBRR0H = (unsigned char)(ubrr >> 8); // UBRR's High bits
+	UBRR0L = (unsigned char)(ubrr);		 // UBRR;s Low bits -> UBRR : 103
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0); // Receive Enable, Transmit Enable
+	UCSR0C = (3<<UCSZ0);			// Set Data bits to 8 bits
 }
 
 void USART_Transmit(char data){
-	while(!((UCSR0A)  & (1<<UDRE0)));
+	while(!((UCSR0A)  & (1<<UDRE0))); // Wait for Data Register empty
 	UDR0 = data;
 }
 
@@ -30,51 +30,50 @@ void USART_Transmit_String(char *str){
 }
 
 char USART_Receive(void){
-	while(!(UCSR0A & (1<<RXC0)));
+	while(!(UCSR0A & (1<<RXC0))); // Wait for Receive Buffer is empty
 	return UDR0;
 }
 void LCD_Init(void){
-	USART_Transmit_String("$I\r");
-	USART_Transmit_String("$C\r");
-	USART_Transmit_String("$B,0\r");
-	USART_Transmit_String("$D,1\r");
-	USART_Transmit_String("$L,1\r");
+	USART_Transmit_String("$I\r"); // LCD Initialize
+	USART_Transmit_String("$C\r"); // Clear LCD
+	USART_Transmit_String("$B,0\r"); // Cursor Off
+	USART_Transmit_String("$D,1\r"); // Display On
+	USART_Transmit_String("$L,1\r"); // Back Light On
 }
 
 void LCD_Transmit(int x, int y, char data){
 	USART_Transmit_String("$G,");
-	USART_Transmit((char)x+48);
+	USART_Transmit((char)x+48); // convert integer number x to ascii number
 	USART_Transmit(',');
-	USART_Transmit((char)y+48);
+	USART_Transmit((char)y+48); // convert integer number y to ascii number
 	USART_Transmit('\r');
 	USART_Transmit_String("$T,");
-	USART_Transmit(data);
+	USART_Transmit(data);		// transmit text
 	USART_Transmit('\r');
 }
 
 void LCD_Transmit_String(int x, int y, char *string){
 	USART_Transmit_String("$G,");
-	USART_Transmit((char)x+48);
+	USART_Transmit((char)x+48); // convert integer number x to ascii number
 	USART_Transmit(',');
-	USART_Transmit((char)y+48);
+	USART_Transmit((char)y+48); // convert integer number y to ascii number
 	USART_Transmit('\r');
 	USART_Transmit_String("$T,");
-	USART_Transmit_String(string);
+	USART_Transmit_String(string);	// transmit string
 	USART_Transmit('\r');
 }
 
 void LCD_Transmit_String_Flow(char *string){
-	int string_L = strlen(string);
-	for(int i=0;i<string_L-31;i++){
-		LCD_Transmit_String(1,1,string+i);
-		LCD_Transmit_String(2,1,string+i+16);
+	int string_L = strlen(string);		// get string length
+	for(int i=0;i<string_L-31;i++){		// print string until string finish
+		LCD_Transmit_String(1,1,string+i);	  // string print for x=1
+		LCD_Transmit_String(2,1,string+i+16); // string print for x=2
 		_delay_ms(100);
-//		if(string[i+32] == '\0') break;
 	}
 }
 
 int main(void){
-	USART_Init(MYUBRR);
+	USART_Init(MYUBRR); // USART initialize with Baud rate 9600
 	LCD_Init();
 	char str[] = "Hwang Seon Jeong Department of Electronic Engineering";
 	
